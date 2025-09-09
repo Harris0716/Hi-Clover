56f86aa5cfb0540f371180fc8789e4707919574a
Guan <112753134@g.nccu.edu.tw>
2025-09-10 00:49:38 +0800

    twcc

--- siamese_test.py
+++ siamese_test.py
@@ -70,63 +70,32 @@ for key, value in state_dict.items():
 model.load_state_dict(new_state_dict)
 model.eval()
 
-# # Load train/validation dataset
-# Siamese = GroupedHiCDataset([SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["training"]+ dataset[data_name]["validation"])],
-#              reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
-# test_sampler = SequentialSampler(Siamese)
-# dataloader = DataLoader(Siamese, batch_size=100, sampler = test_sampler)
-
-# # train/validation set
-# distances, labels = test_triplet_by_siamese(model, dataloader)
-
-
-# mx = max(distances)
-# mn = min(distances[distances>0])
-# rng = np.arange(mn, mx, (mx-mn)/200)
-
-# a = plt.hist(distances[(labels==0)],bins=rng,  density=True, label='replicates', alpha=0.5, color='#108690')
-# b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='conditions', alpha=0.5, color='#1D1E4E')
-# intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
-# plt.axvline(intersect, color='k')
-# plt.xticks(np.arange(0, np.ceil(mx), step=5), fontsize=10)
-# plt.legend()
-# plt.title("distance of train and validation from "+ args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
-# plt.ylabel("density")
-# plt.xlabel("euclidean distance of representation")
-# plt.savefig(args.model_infile.split(".ckpt")[0]+"_train_distribution.pdf")
-# plt.close()
-
-#dataset test
-Siamese = GroupedHiCDataset([ SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["test"])],
-             reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
+# Load train/validation dataset
+Siamese = GroupedHiCDataset([SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["training"]+ dataset[data_name]["validation"])],reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
 test_sampler = SequentialSampler(Siamese)
 dataloader = DataLoader(Siamese, batch_size=100, sampler = test_sampler)
 
-# Test the model
+# train/validation set
 distances, labels = test_triplet_by_siamese(model, dataloader)
+
+
 mx = max(distances)
 mn = min(distances[distances>0])
 rng = np.arange(mn, mx, (mx-mn)/200)
 
-# global_rate = sum(((distances<intersect)==(labels==0)) )/len(distances)
-# TR_rate =  sum((distances<intersect) & (labels==0))/sum(labels==0)
-# BC_rate = sum((distances>intersect) & (labels==1) )/sum(labels==1)
-
-# print('global rate: {:.4f}, replicate rate: {:.4f}, condition rate: {:.4f}'
-#             .format(global_rate, TR_rate, BC_rate))
-
-a = plt.hist(distances[(labels==0)],bins=rng, density=True, label='replicates', alpha=0.5, color='#108690')
+a = plt.hist(distances[(labels==0)],bins=rng,  density=True, label='replicates', alpha=0.5, color='#108690')
 b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='conditions', alpha=0.5, color='#1D1E4E')
 intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
 plt.axvline(intersect, color='k')
 plt.xticks(np.arange(0, np.ceil(mx), step=5), fontsize=10)
-plt.title("distance of test from " + args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
-plt.ylabel("density")
-plt.xlabel("euclidean distance of representation")
 plt.legend()
-plt.savefig(args.model_infile.split(".ckpt")[0]+"_test_distribution.pdf")
+plt.title("Distance of Train/Val from "+ args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
+plt.ylabel("Density")
+plt.xlabel("Euclidean Distance of Representation")
+plt.savefig(args.model_infile.split(".ckpt")[0]+"_train_distribution.pdf")
 plt.close()
 
+print('----train/val-----')
 # Separation Index
 rep_density = a[0]
 cond_density = b[0]
@@ -160,3 +129,72 @@ print("Replicate rate: {:.4f}".format(replicate_rate))
 print("Condition rate: {:.4f}".format(condition_rate))
 print("Mean Performance: {:.4f}".format(mean_performance))
 
+
+
+
+
+#dataset test
+Siamese = GroupedHiCDataset([ SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["test"])],
+             reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
+test_sampler = SequentialSampler(Siamese)
+dataloader = DataLoader(Siamese, batch_size=100, sampler = test_sampler)
+
+# Test the model
+distances, labels = test_triplet_by_siamese(model, dataloader)
+mx = max(distances)
+mn = min(distances[distances>0])
+rng = np.arange(mn, mx, (mx-mn)/200)
+
+# global_rate = sum(((distances<intersect)==(labels==0)) )/len(distances)
+# TR_rate =  sum((distances<intersect) & (labels==0))/sum(labels==0)
+# BC_rate = sum((distances>intersect) & (labels==1) )/sum(labels==1)
+
+# print('global rate: {:.4f}, replicate rate: {:.4f}, condition rate: {:.4f}'
+#             .format(global_rate, TR_rate, BC_rate))
+
+a = plt.hist(distances[(labels==0)],bins=rng, density=True, label='replicates', alpha=0.5, color='#108690')
+b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='conditions', alpha=0.5, color='#1D1E4E')
+intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
+plt.axvline(intersect, color='k')
+plt.xticks(np.arange(0, np.ceil(mx), step=5), fontsize=10)
+plt.title("Distance of Test from " + args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
+plt.ylabel("Density")
+plt.xlabel("Euclidean Distance of Representation")
+plt.legend()
+plt.savefig(args.model_infile.split(".ckpt")[0]+"_test_distribution.pdf")
+plt.close()
+
+
+print('----test-----')
+# Separation Index
+rep_density = a[0]
+cond_density = b[0]
+bin_edges = a[1]
+bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
+bin_widths = np.diff(bin_edges)
+
+# overlap area
+overlap = minimum(rep_density, cond_density)
+
+# Separation Index 
+separation_index_simps = simps(overlap, bin_centers)
+separation_index_sum = np.sum(overlap * bin_widths)
+
+print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
+print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
+
+
+
+# mean performance
+replicate_correct = np.sum((distances < intersect) & (labels == 0))
+condition_correct = np.sum((distances >= intersect) & (labels == 1))
+replicate_total = np.sum(labels == 0)
+condition_total = np.sum(labels == 1)
+
+replicate_rate = replicate_correct / replicate_total
+condition_rate = condition_correct / condition_total
+mean_performance = (replicate_rate + condition_rate) / 2
+
+print("Replicate rate: {:.4f}".format(replicate_rate))
+print("Condition rate: {:.4f}".format(condition_rate))
+print("Mean Performance: {:.4f}".format(mean_performance))
