41f1b596997cdc59b8787ebd02f68defaa6a38bd
guan99716 <guan99716@gmail.com>
2025-09-24 10:51:16 +0800

    use GPU

--- pearson.py
+++ pearson.py
@@ -1,6 +1,5 @@
-import numpy as np
-from torch.utils.data import DataLoader, SequentialSampler
 import torch
+from torch.utils.data import DataLoader, SequentialSampler
 import matplotlib.pyplot as plt
 import argparse
 import json
@@ -8,51 +7,60 @@ from scipy.integrate import simpson
 from numpy import minimum
 from HiSiNet.HiCDatasetClass import HiCDatasetDec, GroupedHiCDataset, SiameseHiCDataset
 from HiSiNet.reference_dictionaries import reference_genomes
-from scipy.stats import pearsonr
 
-parser = argparse.ArgumentParser(description='Triplet network testing module (Pearson correlation distance)')
+parser = argparse.ArgumentParser(description='Triplet network testing module (GPU Pearson correlation distance)')
 parser.add_argument('json_file', type=str, help='JSON file containing dataset paths')
 parser.add_argument("data_inputs", nargs='+', help="keys from JSON for datasets to test")
 args = parser.parse_args()
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
 with open(args.json_file) as json_file:
     dataset = json.load(json_file)
 
+# ===== GPU batch Pearson correlation distance =====
+def batch_pearson_distance(batch1, batch2):
+    """
+    batch1, batch2: torch.Tensor, shape (B, N, N)
+    Returns: distance = 1 - Pearson correlation for each pair
+    """
+    B = batch1.shape[0]
+    flat1 = batch1.view(B, -1)
+    flat2 = batch2.view(B, -1)
 
-# ===== Pearson correlation distance =====
-def pearson_distance(mat1, mat2):
-    """Flatten matrices and compute Pearson correlation distance"""
-    m1 = mat1.flatten()
-    m2 = mat2.flatten()
-    corr, _ = pearsonr(m1, m2)
-    return 1 - corr  # distance: smaller means more similar
+    mean1 = flat1.mean(dim=1, keepdim=True)
+    mean2 = flat2.mean(dim=1, keepdim=True)
 
+    num = ((flat1 - mean1) * (flat2 - mean2)).sum(dim=1)
+    denom = torch.sqrt(((flat1 - mean1)**2).sum(dim=1) * ((flat2 - mean2)**2).sum(dim=1))
+    corr = num / denom
+    return 1 - corr  # distance
 
 # ===== Testing function =====
 def test_triplet_by_siamese(dataloader):
-    distances = np.array([])
-    labels = np.array([])
+    distances = []
+    labels = []
 
     for _, data in enumerate(dataloader):
         input1, input2, label = data
-        input1 = input1.cpu().numpy()
-        input2 = input2.cpu().numpy()
+        input1 = input1.to(device)
+        input2 = input2.to(device)
+        label = label.cpu().numpy()
 
-        batch_scores = []
-        for m1, m2 in zip(input1, input2):
-            if m1.ndim == 1:
-                side = int(np.sqrt(len(m1)))
-                m1 = m1.reshape(side, side)
-                m2 = m2.reshape(side, side)
+        # reshape 1D -> NxN if needed
+        if input1.ndim == 3 and input1.shape[2] == 1:
+            N = int(input1.shape[1]**0.5)
+            input1 = input1.view(input1.shape[0], N, N)
+            input2 = input2.view(input2.shape[0], N, N)
 
-            dist = pearson_distance(m1, m2)
-            batch_scores.append(dist)
+        batch_dist = batch_pearson_distance(input1, input2)
+        distances.append(batch_dist.cpu().numpy())
+        labels.append(label)
 
-        distances = np.concatenate((distances, np.array(batch_scores)))
-        labels = np.concatenate((labels, label.cpu().numpy()))
-
+    distances = np.concatenate(distances)
+    labels = np.concatenate(labels)
     return distances, labels
-
 
 # ===== Helper to create Siamese Dataset from paths =====
 def make_siamese_dataset(data_names, mode="trainval"):
@@ -72,8 +80,12 @@ def make_siamese_dataset(data_names, mode="trainval"):
 
 # ===== Train/Validation =====
 Siamese = make_siamese_dataset(args.data_inputs, mode="trainval")
-test_sampler = SequentialSampler(Siamese)
-dataloader = DataLoader(Siamese, batch_size=10, sampler=test_sampler)
+dataloader = DataLoader(
+    Siamese,
+    batch_size=10,
+    sampler=SequentialSampler(Siamese),
+    num_workers=4  # 使用 4 個子進程讀取資料
+)
 
 distances, labels = test_triplet_by_siamese(dataloader)
 
@@ -93,7 +105,7 @@ plt.xlabel("Distance")
 plt.savefig("train_distribution.pdf")
 plt.close()
 
-print('----train/val-----')
+# ===== Separation Index & Performance =====
 rep_density = a[0]
 cond_density = b[0]
 bin_edges = a[1]
@@ -104,9 +116,6 @@ overlap = minimum(rep_density, cond_density)
 separation_index_simps = 1 - simpson(overlap, x=bin_centers)
 separation_index_sum = 1 - np.sum(overlap * bin_widths)
 
-print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
-print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
-
 replicate_correct = np.sum((distances < intersect) & (labels == 0))
 condition_correct = np.sum((distances >= intersect) & (labels == 1))
 replicate_total = np.sum(labels == 0)
@@ -116,6 +125,9 @@ replicate_rate = replicate_correct / replicate_total
 condition_rate = condition_correct / condition_total
 mean_performance = (replicate_rate + condition_rate) / 2
 
+print('----train/val-----')
+print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
+print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
 print("Replicate rate: {:.4f}".format(replicate_rate))
 print("Condition rate: {:.4f}".format(condition_rate))
 print("Mean Performance: {:.4f}".format(mean_performance))
@@ -123,8 +135,7 @@ print("Mean Performance: {:.4f}".format(mean_performance))
 
 # ===== Test =====
 Siamese_test = make_siamese_dataset(args.data_inputs, mode="test")
-test_sampler = SequentialSampler(Siamese_test)
-dataloader = DataLoader(Siamese_test, batch_size=10, sampler=test_sampler)
+dataloader = DataLoader(Siamese_test, batch_size=10, sampler=SequentialSampler(Siamese_test))
 
 distances, labels = test_triplet_by_siamese(dataloader)
 
@@ -144,7 +155,6 @@ plt.legend()
 plt.savefig("test_distribution.pdf")
 plt.close()
 
-print('----test-----')
 rep_density = a[0]
 cond_density = b[0]
 bin_edges = a[1]
@@ -155,9 +165,6 @@ overlap = minimum(rep_density, cond_density)
 separation_index_simps = 1 - simpson(overlap, x=bin_centers)
 separation_index_sum = 1 - np.sum(overlap * bin_widths)
 
-print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
-print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
-
 replicate_correct = np.sum((distances < intersect) & (labels == 0))
 condition_correct = np.sum((distances >= intersect) & (labels == 1))
 replicate_total = np.sum(labels == 0)
@@ -167,6 +174,9 @@ replicate_rate = replicate_correct / replicate_total
 condition_rate = condition_correct / condition_total
 mean_performance = (replicate_rate + condition_rate) / 2
 
+print('----test-----')
+print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
+print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
 print("Replicate rate: {:.4f}".format(replicate_rate))
 print("Condition rate: {:.4f}".format(condition_rate))
 print("Mean Performance: {:.4f}".format(mean_performance))
