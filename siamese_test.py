71e0cf0560b6fbfac2361078a4ef04c16cff57f5
Guan <112753134@g.nccu.edu.tw>
2025-10-15 03:36:38 +0800

    show pdf

--- siamese_test.py
+++ siamese_test.py
@@ -1,8 +1,8 @@
 import numpy as np
-from torch.utils.data import DataLoader, SequentialSampler
+from torch.utils.data import Dataset, DataLoader, SequentialSampler
 import torch.nn.functional as F
 import torch.nn as nn
-from HiSiNet.HiCDatasetClass import HiCDatasetDec, GroupedHiCDataset, SiameseHiCDataset
+from HiSiNet.HiCDatasetClass import HiCDatasetDec, TripletHiCDataset, GroupedHiCDataset, SiameseHiCDataset
 import HiSiNet.models as models
 import torch
 import matplotlib.pyplot as plt
@@ -11,8 +11,6 @@ from HiSiNet.reference_dictionaries import reference_genomes
 import json
 from scipy.integrate import simpson
 from numpy import minimum
-import csv
-import os
 
 parser = argparse.ArgumentParser(description='Triplet network testing module')
 parser.add_argument('model_name', type=str, help='a string indicating a model from models')
@@ -36,101 +34,144 @@ def test_triplet_by_siamese(model, dataloader):
             input1, input2 = input1.to(cuda), input2.to(cuda)
             label = label.type(torch.FloatTensor).to(cuda)
             output1, output2, _ = model(input1, input2, input2)
-            predicted = F.pairwise_distance(output1, output2)
+            predicted = F.pairwise_distance(output1,output2)
             distances = np.concatenate((distances, predicted.cpu().detach().numpy()))
+            label = label.type(torch.FloatTensor)
             labels  = np.concatenate((labels, label.cpu().detach().numpy()))
 
     return distances, labels
 
-def compute_metrics(distances, labels, intersect):
-    rep_correct = np.sum((distances < intersect) & (labels == 0))
-    cond_correct = np.sum((distances >= intersect) & (labels == 1))
-    rep_total = np.sum(labels == 0)
-    cond_total = np.sum(labels == 1)
-
-    rep_rate = rep_correct / rep_total if rep_total > 0 else 0
-    cond_rate = cond_correct / cond_total if cond_total > 0 else 0
-    mean_perf = (rep_rate + cond_rate) / 2
-
-    # hist densities for separation index
-    rng = np.linspace(min(distances[distances>0]), max(distances), 200)
-    a = np.histogram(distances[(labels==0)], bins=rng, density=True)
-    b = np.histogram(distances[(labels==1)], bins=rng, density=True)
-
-    rep_density = a[0]
-    cond_density = b[0]
-    bin_edges = a[1]
-    bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
-    bin_widths = np.diff(bin_edges)
-
-    overlap = minimum(rep_density, cond_density)
-    sep_simps = 1 - simpson(overlap, x=bin_centers)
-    sep_sum = 1 - np.sum(overlap * bin_widths)
-
-    return sep_simps, sep_sum, rep_rate, cond_rate, mean_perf
-
 cuda = torch.device("cuda:0")
 model = eval("models."+ args.model_name)(mask=args.mask).to(cuda)
 state_dict = torch.load(args.model_infile, weights_only=True, map_location=cuda)
-new_state_dict = {key.replace("module.", ""): value for key, value in state_dict.items()}
+new_state_dict = {}
+for key, value in state_dict.items():
+    new_key = key.replace("module.", "")  # remove 'module.' prefix
+    new_state_dict[new_key] = value
 model.load_state_dict(new_state_dict)
 model.eval()
 
-# --------- 準備 CSV 檔案 ---------
-csv_file = "results.csv"
-write_header = not os.path.exists(csv_file)
-with open(csv_file, "a", newline="") as f:
-    writer = csv.writer(f)
-    if write_header:
-        writer.writerow([
-            "Split",
-            "SeparationIndex_Simpson", "SeparationIndex_Sum",
-            "ReplicateRate", "ConditionRate", "MeanPerformance"
-        ])
-# --------------------------------
+# Load train/validation dataset
+Siamese = GroupedHiCDataset([SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["training"]+ dataset[data_name]["validation"])],reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
+test_sampler = SequentialSampler(Siamese)
+dataloader = DataLoader(Siamese, batch_size=100, sampler = test_sampler)
 
-# train/val
-Siamese = GroupedHiCDataset([
-    SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["training"] + dataset[data_name]["validation"])],
-    reference=reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs
-])
-dataloader = DataLoader(Siamese, batch_size=100, sampler=SequentialSampler(Siamese))
-
+# train/validation set
 distances, labels = test_triplet_by_siamese(model, dataloader)
-a = plt.hist(distances[(labels==0)], bins=200, density=True)
-b = plt.hist(distances[(labels==1)], bins=200, density=True)
+
+
+mx = max(distances)
+mn = min(distances[distances>0])
+rng = np.arange(mn, mx, (mx-mn)/200)
+
+a = plt.hist(distances[(labels==0)],bins=rng,  density=True, label='replicates', alpha=0.5, color='#108690')
+b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='conditions', alpha=0.5, color='#1D1E4E')
 intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
+plt.axvline(intersect, color='k')
+plt.xticks(np.arange(0, np.ceil(mx), step=5), fontsize=10)
+plt.legend()
+plt.title("Distance of Train/Val from "+ args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
+plt.ylabel("Density")
+plt.xlabel("Euclidean Distance of Representation")
+plt.savefig(args.model_infile.split(".ckpt")[0]+"_train_distribution.pdf")
 plt.close()
 
-sep_simps, sep_sum, rep_rate, cond_rate, mean_perf = compute_metrics(distances, labels, intersect)
+print('----train/val-----')
+# Separation Index
+rep_density = a[0]
+cond_density = b[0]
+bin_edges = a[1]
+bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
+bin_widths = np.diff(bin_edges)
 
-with open(csv_file, "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow([
-        "train/val",
-        "{:.4f}".format(sep_simps), "{:.4f}".format(sep_sum),
-        "{:.4f}".format(rep_rate), "{:.4f}".format(cond_rate), "{:.4f}".format(mean_perf)
-    ])
+# overlap area
+overlap = minimum(rep_density, cond_density)
 
-# test
-Siamese = GroupedHiCDataset([
-    SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in dataset[data_name]["test"]],
-    reference=reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs
-])
-dataloader = DataLoader(Siamese, batch_size=100, sampler=SequentialSampler(Siamese))
+# Separation Index 
+separation_index_simps = 1 - simpson(overlap, x = bin_centers)
+separation_index_sum = 1 - np.sum(overlap * bin_widths)
 
+print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
+print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
+
+
+
+# mean performance
+replicate_correct = np.sum((distances < intersect) & (labels == 0))
+condition_correct = np.sum((distances >= intersect) & (labels == 1))
+replicate_total = np.sum(labels == 0)
+condition_total = np.sum(labels == 1)
+
+replicate_rate = replicate_correct / replicate_total
+condition_rate = condition_correct / condition_total
+mean_performance = (replicate_rate + condition_rate) / 2
+
+print("Replicate rate: {:.4f}".format(replicate_rate))
+print("Condition rate: {:.4f}".format(condition_rate))
+print("Mean Performance: {:.4f}".format(mean_performance))
+
+#dataset test
+Siamese = GroupedHiCDataset([ SiameseHiCDataset([HiCDatasetDec.load(data_path) for data_path in (dataset[data_name]["test"])],
+             reference = reference_genomes[dataset[data_name]["reference"]]) for data_name in args.data_inputs] )
+test_sampler = SequentialSampler(Siamese)
+dataloader = DataLoader(Siamese, batch_size=100, sampler = test_sampler)
+
+# Test the model
 distances, labels = test_triplet_by_siamese(model, dataloader)
-a = plt.hist(distances[(labels==0)], bins=200, density=True)
-b = plt.hist(distances[(labels==1)], bins=200, density=True)
+mx = max(distances)
+mn = min(distances[distances>0])
+rng = np.arange(mn, mx, (mx-mn)/200)
+
+# global_rate = sum(((distances<intersect)==(labels==0)) )/len(distances)
+# TR_rate =  sum((distances<intersect) & (labels==0))/sum(labels==0)
+# BC_rate = sum((distances>intersect) & (labels==1) )/sum(labels==1)
+
+# print('global rate: {:.4f}, replicate rate: {:.4f}, condition rate: {:.4f}'
+#             .format(global_rate, TR_rate, BC_rate))
+
+a = plt.hist(distances[(labels==0)],bins=rng, density=True, label='replicates', alpha=0.5, color='#108690')
+b = plt.hist(distances[(labels==1)],bins=rng, density=True, label='conditions', alpha=0.5, color='#1D1E4E')
 intersect = a[1][np.argwhere(np.diff(np.sign(a[0]-b[0])))[0]]
+plt.axvline(intersect, color='k')
+plt.xticks(np.arange(0, np.ceil(mx), step=5), fontsize=10)
+plt.title("Distance of Test from " + args.model_infile.split("/")[-1] +" on: "+ ", ".join(args.data_inputs))
+plt.ylabel("Density")
+plt.xlabel("Euclidean Distance of Representation")
+plt.legend()
+plt.savefig(args.model_infile.split(".ckpt")[0]+"_test_distribution.pdf")
 plt.close()
 
-sep_simps, sep_sum, rep_rate, cond_rate, mean_perf = compute_metrics(distances, labels, intersect)
 
-with open(csv_file, "a", newline="") as f:
-    writer = csv.writer(f)
-    writer.writerow([
-        "test",
-        "{:.4f}".format(sep_simps), "{:.4f}".format(sep_sum),
-        "{:.4f}".format(rep_rate), "{:.4f}".format(cond_rate), "{:.4f}".format(mean_perf)
-    ])
+print('----test-----')
+# Separation Index
+rep_density = a[0]
+cond_density = b[0]
+bin_edges = a[1]
+bin_centers = (bin_edges[1:] + bin_edges[:-1]) / 2
+bin_widths = np.diff(bin_edges)
+
+# overlap area
+overlap = minimum(rep_density, cond_density)
+
+# Separation Index 
+separation_index_simps = 1 - simpson(overlap, x = bin_centers)
+separation_index_sum = 1 - np.sum(overlap * bin_widths)
+
+print("Separation Index (Simpson): {:.4f}".format(separation_index_simps))
+print("Separation Index (Sum)    : {:.4f}".format(separation_index_sum))
+
+
+
+# mean performance
+replicate_correct = np.sum((distances < intersect) & (labels == 0))
+condition_correct = np.sum((distances >= intersect) & (labels == 1))
+replicate_total = np.sum(labels == 0)
+condition_total = np.sum(labels == 1)
+
+replicate_rate = replicate_correct / replicate_total
+condition_rate = condition_correct / condition_total
+mean_performance = (replicate_rate + condition_rate) / 2
+
+print("Replicate rate: {:.4f}".format(replicate_rate))
+print("Condition rate: {:.4f}".format(condition_rate))
+print("Mean Performance: {:.4f}".format(mean_performance))
