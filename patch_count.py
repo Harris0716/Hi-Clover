7c96315300a22ef733ea40861d10343f06409eee
Guan <112753134@g.nccu.edu.tw>
2025-12-03 02:22:59 +0800

    patch count

--- patch_count.py
+++ patch_count.py
@@ -1,5 +1,11 @@
 from HiSiNet.HiCDatasetClass import HiCDatasetDec
 
-dataset = HiCDatasetDec.load("input.mlhic")
-patch, label = dataset[0] 
-patch = patch.numpy()
+dataset_train = HiCDatasetDec.load("liver_data/TAM_R1.mlhic")
+dataset_val = HiCDatasetDec.load("liver_data/TAM_validation_R1.mlhic")
+dataset_test = HiCDatasetDec.load("liver_data/TAM_test_R1.mlhic")
+
+print(len(dataset_train))
+print(len(dataset_val))
+print(len(dataset_test))
+
+print('total patches: ', len(dataset_train) + len(dataset_val) + len(dataset_test))
