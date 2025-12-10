c467dc14cd54fe1f750fa87f4d23fc79ced4c111
guan99716 <guan99716@gmail.com>
2025-12-10 16:04:38 +0800

    args.lr

--- train.py
+++ train.py
@@ -105,7 +105,7 @@ model = model.to(device)
 model_save_path = args.outpath + args.model_name + '_' + str(learning_rate) + '_' + str(batch_size) + '_' + str(args.seed)
 
 criterion = TripletLoss(margin=args.margin)
-optimizer = optim.Adam(model.parameters(), lr=0.001) 
+optimizer = optim.Adam(model.parameters(), lr=args.learning_rate) 
 
 train_losses = []
 val_losses = []
