d483c21d84aa69c462c22b8144d851c19a605f91
guan99716 <guan99716@gmail.com>
2026-01-03 16:12:35 +0800

    add shell file

---
+++ run_seeds.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+SEEDS=(42 123 1025 2026 7)
+MODEL="TripletLeNet"
+LR=0.001
+MARGIN=0.5
+
+for SEED in "${SEEDS[@]}"
+do
+    OUT_DIR="./outputs_seeds/seed_${SEED}/"
+    mkdir -p $OUT_DIR
+
+    # 1. 訓練
+    python Hi-Clover/train_v2.py $MODEL config.json $LR NIPBL \
+        --outpath $OUT_DIR --seed $SEED --margin $MARGIN --mask true
+
+    # 2. 定義模型路徑 (需與 train_v2.py 產生的檔名一致)
+    CKPT="${OUT_DIR}${MODEL}_${LR}_128_${SEED}_${MARGIN}_best.ckpt"
+
+    # 3. 測試
+    python Hi-Clover/test.py $MODEL config.json $CKPT NIPBL --mask true
+done
