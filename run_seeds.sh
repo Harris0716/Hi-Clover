f95eb3b042238620f5e858a0280a0c37cf059bd7
guan99716 <guan99716@gmail.com>
2026-01-05 21:17:24 +0800

    batch

--- run_seeds.sh
+++ run_seeds.sh
@@ -3,6 +3,7 @@ SEEDS=(42 123 1025 2026 7)
 MODEL="TripletLeNet"
 LR=0.001
 MARGIN=0.5
+BATCH = 128
 
 for SEED in "${SEEDS[@]}"
 do
@@ -14,7 +15,7 @@ do
         --outpath $OUT_DIR --seed $SEED --margin $MARGIN --mask true
 
     # 2. 定義模型路徑 (需與 train_v2.py 產生的檔名一致)
-    CKPT="${OUT_DIR}${MODEL}_${LR}_128_${SEED}_${MARGIN}_best.ckpt"
+    CKPT="${OUT_DIR}${MODEL}_${LR}_${BATCH}_${SEED}_${MARGIN}_best.ckpt"
 
     # 3. 測試
     python Hi-Clover/test.py $MODEL config.json $CKPT NIPBL --mask true
