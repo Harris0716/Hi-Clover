c8611b1d0bbe197ec0c3583a2b5d9066c9828568
guan99716 <guan99716@gmail.com>
2026-01-12 22:56:54 +0800

    change to 4 triplet

--- HiSiNet/HiCDatasetClass.py
+++ HiSiNet/HiCDatasetClass.py
@@ -138,51 +138,10 @@ class TripletHiCDataset(HiCDataset):
             filenames_norm.add((data.metadata['filename'], data.metadata['norm']))
         return True
 
-    def append_data(self, curr_data, pos):
-        """生成 triplets，確保每個樣本都擔任過 Anchor (共 8 個)"""
-        if len(curr_data) < 2:
-            return
-        class_groups = {}
-        for i, data in enumerate(curr_data):
-            class_id = data[1]
-            if class_id not in class_groups:
-                class_groups[class_id] = []
-            class_groups[class_id].append(data)
-
-        for anchor_class, samples in class_groups.items():
-            if len(samples) < 2:
-                continue
-
-            # 遍歷該類別中的每一個樣本作為 Anchor
-            for i in range(len(samples)):
-                anchor = samples[i]
-                
-                # 遍歷同類別中的「其他」樣本作為 Positive
-                for j in range(len(samples)):
-                    if i == j: continue # 不能自己跟自己配對
-                    positive = samples[j]
-
-                    # 遍歷不同類別的樣本作為 Negative
-                    for other_class, other_samples in class_groups.items():
-                        if other_class == anchor_class:
-                            continue
-                        
-                        for negative in other_samples:
-                            self.data.append((
-                                anchor[0],    
-                                positive[0],  
-                                negative[0],  
-                                anchor_class  
-                            ))
-                            self.positions.append(pos)
-                            self.labels.append((anchor_class, other_class))
-
     # def append_data(self, curr_data, pos):
-    #     """Generate triplets from current data."""
+    #     """生成 triplets，確保每個樣本都擔任過 Anchor (共 8 個)"""
     #     if len(curr_data) < 2:
     #         return
-
-    #     # Group data by class_id
     #     class_groups = {}
     #     for i, data in enumerate(curr_data):
     #         class_id = data[1]
@@ -190,26 +149,67 @@ class TripletHiCDataset(HiCDataset):
     #             class_groups[class_id] = []
     #         class_groups[class_id].append(data)
 
-    #     # Generate triplets
-    #     for anchor_class in class_groups:
-    #         if len(class_groups[anchor_class]) < 2:
+    #     for anchor_class, samples in class_groups.items():
+    #         if len(samples) < 2:
     #             continue
 
-    #         for i, anchor in enumerate(class_groups[anchor_class]):
-    #             for j in range(i + 1, len(class_groups[anchor_class])):
-    #                 positive = class_groups[anchor_class][j]
-    #                 for other_class in class_groups:
-    #                     if other_class != anchor_class:
-    #                         for negative in class_groups[other_class]:
-    #                             # --- 關鍵修改：將 anchor_class (也就是 Label) 存入 ---
-    #                             self.data.append((
-    #                                 anchor[0],    # anchor image
-    #                                 positive[0],  # positive image
-    #                                 negative[0],  # negative image
-    #                                 anchor_class  # <--- 新增這一行
-    #                             ))
-    #                             self.positions.append(pos)
-    #                             self.labels.append((anchor_class, other_class))
+    #         # 遍歷該類別中的每一個樣本作為 Anchor
+    #         for i in range(len(samples)):
+    #             anchor = samples[i]
+                
+    #             # 遍歷同類別中的「其他」樣本作為 Positive
+    #             for j in range(len(samples)):
+    #                 if i == j: continue # 不能自己跟自己配對
+    #                 positive = samples[j]
+
+    #                 # 遍歷不同類別的樣本作為 Negative
+    #                 for other_class, other_samples in class_groups.items():
+    #                     if other_class == anchor_class:
+    #                         continue
+                        
+    #                     for negative in other_samples:
+    #                         self.data.append((
+    #                             anchor[0],    
+    #                             positive[0],  
+    #                             negative[0],  
+    #                             anchor_class  
+    #                         ))
+    #                         self.positions.append(pos)
+    #                         self.labels.append((anchor_class, other_class))
+
+    def append_data(self, curr_data, pos):
+        """Generate triplets from current data."""
+        if len(curr_data) < 2:
+            return
+
+        # Group data by class_id
+        class_groups = {}
+        for i, data in enumerate(curr_data):
+            class_id = data[1]
+            if class_id not in class_groups:
+                class_groups[class_id] = []
+            class_groups[class_id].append(data)
+
+        # Generate triplets
+        for anchor_class in class_groups:
+            if len(class_groups[anchor_class]) < 2:
+                continue
+
+            for i, anchor in enumerate(class_groups[anchor_class]):
+                for j in range(i + 1, len(class_groups[anchor_class])):
+                    positive = class_groups[anchor_class][j]
+                    for other_class in class_groups:
+                        if other_class != anchor_class:
+                            for negative in class_groups[other_class]:
+                                # --- 關鍵修改：將 anchor_class (也就是 Label) 存入 ---
+                                self.data.append((
+                                    anchor[0],    # anchor image
+                                    positive[0],  # positive image
+                                    negative[0],  # negative image
+                                    anchor_class  # <--- 新增這一行
+                                ))
+                                self.positions.append(pos)
+                                self.labels.append((anchor_class, other_class))
 
     def make_data(self, list_of_HiCDatasets):
         """Process datasets and generate triplets."""
