4e3a213e162c80ca9bdb2836c999648867b155f0
Guan <112753134@g.nccu.edu.tw>
2026-01-20 15:37:05 +0800

    fix data

--- TEST_ID.py
+++ TEST_ID.py
@@ -4,5 +4,6 @@ from HiSiNet.HiCDatasetClass import HiCDatasetDec
 tam = HiCDatasetDec.load('/work/u1696810/liver_data/TAM_R1.mlhic') 
 nipbl = HiCDatasetDec.load('/work/u1696810/liver_data/NIPBL_R1.mlhic') 
 # 印出 class_id
-print(f"TAM 的 class_id 是: {data.metadata['class_id']}") 
-print(f"NIPBL 的 class_id 是: {data.metadata['class_id']}") 
+for data in [tam, nipbl]:
+    print(f"TAM 的 class_id 是: {data.metadata['class_id']}") 
+    print(f"NIPBL 的 class_id 是: {data.metadata['class_id']}") 
